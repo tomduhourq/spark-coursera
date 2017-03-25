57dd0993d47f1ef040d08fba15bda33222978c22
tomduhourq <t.duhourq@gmail.com>
2017-03-25 19:06:10 -0300

    Final assignment: 8/10

--- stackoverflow/src/main/scala/stackoverflow/StackOverflow.scala
+++ stackoverflow/src/main/scala/stackoverflow/StackOverflow.scala
@@ -1,7 +1,6 @@
 package stackoverflow
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
@@ -293,9 +292,6 @@ class StackOverflow extends Serializable {
     }
     ((comp1 / count).toInt, (comp2 / count).toInt)
   }
-
-
-
 
   //
   //
